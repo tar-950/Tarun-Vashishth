e7d5650bbd6ba439b40e972e37084166400463db
KailashChandra-dotcom <60435678+KailashChandra-dotcom@users.noreply.github.com>
2020-04-21 15:10:04 +0530

    Update ranking.py

--- final_top_resume-master/ranking.py
+++ final_top_resume-master/ranking.py
@@ -1,37 +1,36 @@
 #importing pandas
 import pandas as pd
 
-def filterRanked(data,skill_input,exp_input):
+def filterRanked(data,key_list,experience_min,experience_max):
     #user required skills
-    keylist = skill_input
+    keylist = key_list
     list_with_index= []
-    len_key = len(keylist)
+    len_list = len(keylist)
 
     #user required experience
-    req_experience = int(exp_input)
+    req_experience_min = experience_min
+    req_experience_max = experience_max
 
     list_with_rating=[]
 
-
     for dict in data:
-        
+        total_rating,count=0,0        
         experience_have = dict.get('Experience')
 
-        if req_experience <= experience_have:
+        if req_experience_min <= experience_have and req_experience_max>=experience_have:
             exp_rate = 10
             # index_of_dict_in_list.append(data.index(dict))
             skill_list = dict.get('Skills')
-            total_rating,count=0,0
 
             for item in keylist:
                 if item in skill_list:
                     count+=1
 
-            if count !=0:
-                diff = len_key - count
-                skill_rate = 10 - ((len_key/10) * diff)
+            if count!= 0:
+                diff = len_list - count
+                skill_rate = 10 - ((10/len_list) * diff)
 
-                if count == len_key:
+                if count == len_list:
                     #fetching skills from our data and rate them
                     total_skill = dict.get('Skill_Count')
                     left_skill_rate = (total_skill-count)/5
@@ -50,10 +49,8 @@ def filterRanked(data,skill_input,exp_input):
         list_with_rating.append(data[index])
 
     sorted_list_with_rating = sorted(list_with_rating, key = lambda i: (i['Rating'],i['Experience'],i['Skill_Count']), reverse= True)
-    # sorted_list_with_rating = sorted(list_with_rating, key = lambda i:i['Experience'])
-    # sorted_list_with_rating = sorted(list_with_rating, key = lambda i:i['Skill_Count'], reverse = True)
+
     df = pd.DataFrame(sorted_list_with_rating)
     df.to_csv('data1.csv')
     print('done')
-    # print(sorted_list_with_rating)
     return sorted_list_with_rating
